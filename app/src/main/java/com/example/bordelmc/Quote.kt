01b602390e8c5bf4520888b23b69b5203f443dab
AlexQobuz <alexandre.clemencot@qobuz.com>
2023-11-28 17:23:57 +0100

    model Quote + extract title in string ressource

---
+++ app/src/main/java/com/example/bordelmc/Quote.kt
@@ -0,0 +1,7 @@
+package com.example.bordelmc
+
+//'https://dummyjson.com/products'
+data class Quote(
+    val id: Int,
+    val title: String
+)
--- app/src/main/java/com/example/bordelmc/ui/component/AppBar.kt
+++ app/src/main/java/com/example/bordelmc/ui/component/AppBar.kt
@@ -1,20 +1,24 @@
 package com.example.bordelmc.ui.component
 
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.bordelmc.R
+import com.example.bordelmc.ui.theme.Blue
 import com.example.bordelmc.ui.theme.BordelMcTheme
+import com.example.bordelmc.ui.theme.White
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AppBar(
-    modifier: Modifier = Modifier
-        .fillMaxWidth(),
-    title: String = "",
+
 ) {
-    AppBar(
-        modifier = Modifier,
-        title = "BordelMc",
+    TopAppBar(
+        title = { stringResource(R.string.appTitle) },
+        colors = TopAppBarDefaults.smallTopAppBarColors(titleContentColor = White, containerColor = Blue)
     )
 }
 
