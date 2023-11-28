d2d179e35a882455e70b7713232549dc33d84d1c
AlexQobuz <alexandre.clemencot@qobuz.com>
2023-11-28 15:53:48 +0100

    Initial commit

---
+++ app/src/main/java/com/example/bordelmc/ui/theme/Color.kt
@@ -0,0 +1,54 @@
+package com.example.bordelmc.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Black = Color(0xFF000000)
+
+val White = Color(0xFFFFFFFF)
+
+val LightGrey = Color(0xFFFBFBFB)
+
+val MediumGrey = Color(0xFF939393)
+
+val Grey40 = Color(0xFFBDBDBD)
+val Grey50 = Color(0xFF9E9E9E)
+val Grey70 = Color(0xFF616161)
+val Grey80 = Color(0xFF424242)
+val Grey90 = Color(0xFF323232)
+val Grey100 = Color(0xFF242424)
+val Grey110 = Color(0xFF181818)
+
+val DarkGrey100 = Color(0xFF333333)
+val DarkGrey200 = Color(0xFF262626)
+val DarkGrey300 = Color(0xFF1E1E1E)
+val DarkGrey350 = Color(0xFF181818)
+val DarkGrey400 = Color(0xFF121212)
+
+val Blue = Color(0xFF0070EF)
+
+val Red = Color(0xFFD1404D)
+val Red100 = Color(0xFFE08088)
+val Red200 = Color(0xFFFBEEEF)
+
+val Green = Color(0xFF228C69)
+val Green100 = Color(0xFF6CB29B)
+val Green200 = Color(0xFFE4EEEA)
+
+val SublimeGold = Color(0xFFFFB12F)
+val Brand80 = Color(0xFFFFCB74)
+
+val WEEKLY_COLORS = listOf(
+    Color(0xFF803E6A) to Color(0xFFBE88AC),
+    Color(0xFFBF4647) to Color(0xFFF39FA3),
+    Color(0xFF424983) to Color(0xFF8996BF),
+    Color(0xFF8A1772) to Color(0xFFBE5CAF),
+    Color(0xFF44A470) to Color(0xFF75CD9D)
+)
