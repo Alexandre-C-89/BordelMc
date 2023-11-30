862edd1592400c571165735c6ea2c1701aa93826
AlexQobuz <alexandre.clemencot@qobuz.com>
2023-11-30 17:17:26 +0100

    Add BottomBar

---
+++ app/src/main/java/com/example/bordelmc/ui/component/AppScaffold.kt
@@ -0,0 +1,37 @@
+package com.example.bordelmc.ui.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.consumedWindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.bordelmc.ui.theme.Blue
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class)
+@Composable
+fun AppScaffold(
+    modifier: Modifier = Modifier,
+    topBar: @Composable () -> Unit = {},
+    bottomBar: @Composable () -> Unit = {},
+    content: @Composable () -> Unit
+) {
+    Scaffold(
+        topBar = topBar,
+        modifier = modifier,
+        containerColor = Blue,
+        bottomBar = bottomBar
+    ){ innerPadding ->
+        Box(
+            modifier = Modifier
+                .padding(innerPadding)
+                .consumedWindowInsets(innerPadding)
+                .fillMaxSize()
+        ) {
+            content()
+        }
+    }
+}
--- app/src/main/java/com/example/bordelmc/ui/home/Home.kt
+++ app/src/main/java/com/example/bordelmc/ui/home/Home.kt
@@ -1,16 +1,10 @@
 package com.example.bordelmc.ui.home
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import com.example.bordelmc.ui.component.AppBar
+import com.example.bordelmc.ui.component.AppBottomBar
+import com.example.bordelmc.ui.component.AppScaffold
 
 @Composable
 fun HomeRoute(
@@ -19,21 +13,15 @@ fun HomeRoute(
     HomeScreen()
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun HomeScreen(
 
 ) {
-    Scaffold(
-        topBar = { AppBar() }
+    AppScaffold(
+        topBar = { AppBar() },
+        bottomBar = { AppBottomBar() }
     ) {
-        Box(
-            modifier = Modifier.fillMaxSize()
-        ) {
-            Row(modifier = Modifier.fillMaxWidth()) {
 
-            }
-        }
     }
 }
