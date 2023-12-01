d5a68b91ba6912471e7701d5303b06be896b8970
AlexQobuz <alexandre.clemencot@qobuz.com>
2023-12-01 14:40:05 +0100

    App With call API

--- app/src/main/java/com/example/bordelmc/MainActivity.kt
+++ app/src/main/java/com/example/bordelmc/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.example.bordelmc
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.bordelmc.ui.home.HomeRoute
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import com.example.bordelmc.ui.theme.BordelMcTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val vm = TodoViewModel()
         super.onCreate(savedInstanceState)
         setContent {
             BordelMcTheme {
@@ -23,9 +42,65 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    HomeRoute()
+                    TodoView(vm)
                 }
             }
         }
     }
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TodoView(vm: TodoViewModel) {
+
+    LaunchedEffect(Unit, block = {
+        vm.getTodoList()
+    })
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Row {
+                        Text("Todos")
+                    }
+                })
+        },
+        content = {
+            if (vm.errorMessage.isEmpty()) {
+                Column(modifier = Modifier.padding(16.dp)) {
+                    LazyColumn(modifier = Modifier.fillMaxHeight()) {
+                        items(vm.todoList) { todo ->
+                            Column {
+                                Row(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(16.dp),
+                                    horizontalArrangement = Arrangement.SpaceBetween
+                                ) {
+                                    Box(
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .padding(0.dp, 0.dp, 16.dp, 0.dp)
+                                    ) {
+                                        Text(
+                                            todo.title,
+                                            maxLines = 1,
+                                            overflow = TextOverflow.Ellipsis
+                                        )
+                                    }
+                                    Spacer(modifier = Modifier.width(16.dp))
+                                    Checkbox(checked = todo.completed, onCheckedChange = null)
+                                }
+                                Divider()
+                            }
+                        }
+                    }
+                }
+            } else {
+                Text(vm.errorMessage)
+            }
+        }
+    )
 }
