d4b6c5e770a1e01fda42e35dc66dd21af571d607
AlexQobuz <alexandre.clemencot@qobuz.com>
2023-12-22 14:59:13 +0100

    not good idea

--- app/src/main/java/com/example/bordelmc/data/repository/JsonPlaceholderRepository.kt
+++ app/src/main/java/com/example/bordelmc/data/repository/JsonPlaceholderRepository.kt
@@ -1,4 +1,19 @@
 package com.example.bordelmc.data.repository
 
-class JsonPlaceholderRepository {
+import com.example.bordelmc.data.datasource.ApiService
+import com.example.bordelmc.data.utils.Resource
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class JsonPlaceholderRepository @Inject constructor(
+    private val api: ApiService
+) {
+    suspend fun getNotes() = flow{
+        emit(Resource.Loading())
+        val quotes = api.getQuotes().body()?.quotes
+        emit(Resource.Success(quotes))
+    }.catch { error->
+        emit(Resource.Error(error.message!!))
+    }
 }
--- app/src/main/java/com/example/bordelmc/data/utils/Constants.kt
+++ app/src/main/java/com/example/bordelmc/data/utils/Constants.kt
@@ -3,4 +3,5 @@ package com.example.bordelmc.data.utils
 object Constants {
     val BASE_URL_QUOTE = "https://dummyjson.com/"
     // Add some other url
+    val BASE_URL_JSON = "https://jsonplaceholder.typicode.com/"
 }
